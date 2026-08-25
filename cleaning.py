283d5270a5979284f584e81fa006fe43bfc81809
Himanshu Sharma <himanshush0001@gmail.com>
2026-08-25 13:29:28 +0530

    update

--- cleaning.py
+++ cleaning.py
@@ -4,62 +4,62 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("data/shipments_train.csv")
 
-# print(df.shape)
-# print(df.dtypes)
-# print(df.isnull().sum())
-# print(df.duplicated().sum())
-# print(df["is_late"].value_counts())
-# print(df["is_late"].value_counts(normalize=True))
+print(df.shape)
+print(df.dtypes)
+print(df.isnull().sum())
+print(df.duplicated().sum())
+print(df["is_late"].value_counts())
+print(df["is_late"].value_counts(normalize=True))
 
-# # basic
-# print(df.describe())
+# basic
+print(df.describe())
 
 # late rate by courier
 
-# courier_late_rate = (df.groupby("courier_partner")["is_late"].mean().sort_values(ascending=False))
+courier_late_rate = (df.groupby("courier_partner")["is_late"].mean().sort_values(ascending=False))
 
-# plt.figure(figsize=(8,5))
-# courier_late_rate.plot(kind="bar")
+plt.figure(figsize=(8,5))
+courier_late_rate.plot(kind="bar")
 
-# plt.title("Late rate by courier partner")
-# plt.xlabel("courier partner")
-# plt.ylabel("late rate")
-# plt.xticks(rotation=30, ha="right")
-# plt.tight_layout()
+plt.title("Late rate by courier partner")
+plt.xlabel("courier partner")
+plt.ylabel("late rate")
+plt.xticks(rotation=30, ha="right")
+plt.tight_layout()
 
-# plt.show()
+plt.show()
 
 # late rate by promised days
 
 
-# promised_late_rate=(df.groupby("promised_days")["is_late"].mean())
+promised_late_rate=(df.groupby("promised_days")["is_late"].mean())
 
-# plt.figure(figsize=(7,5))
+plt.figure(figsize=(7,5))
 
-# promised_late_rate.plot(kind="bar")
+promised_late_rate.plot(kind="bar")
 
-# plt.title("Late rate by promised deliverity days")
-# plt.xlabel("Promised days")
-# plt.ylabel("late rate")
-# plt.xticks(rotation=0)
-# plt.tight_layout()
+plt.title("Late rate by promised deliverity days")
+plt.xlabel("Promised days")
+plt.ylabel("late rate")
+plt.xticks(rotation=0)
+plt.tight_layout()
 
-# plt.show()
+plt.show()
 
 # late vs on-time
 
-# plt.figure(figsize=(8,5))
-# plt.hist(df[df["is_late"] == 0]["distance_km"].dropna(),bins=30,alpha=0.6,label="on time")
+plt.figure(figsize=(8,5))
+plt.hist(df[df["is_late"] == 0]["distance_km"].dropna(),bins=30,alpha=0.6,label="on time")
 
-# plt.hist(df[df["is_late"]== 1]["distance_km"].dropna(),bins=30,alpha=0.6,label="Late")
+plt.hist(df[df["is_late"]== 1]["distance_km"].dropna(),bins=30,alpha=0.6,label="Late")
 
-# plt.title("Distance distribution : late vs on time")
-# plt.xlabel("distance")
-# plt.ylabel("number of shipments")
-# plt.legend()
-# plt.tight_layout()
+plt.title("Distance distribution : late vs on time")
+plt.xlabel("distance")
+plt.ylabel("number of shipments")
+plt.legend()
+plt.tight_layout()
 
-# plt.show()
+plt.show()
 
 # late rate by month of dispatch
 
